1a06da9b4bd0fb5489e6d990445d8b19f6145960
Tyler Parmelee <tpparmelee@gmail.com>
2022-01-27 21:01:37 -0500

    ADD METhod to push to source control

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ RetrieveFiles.getFilesByUser = function(user) {
             },
         }
     ).then((response) => {
-        this.retrieveXMLRecords(response.data.result);
+        this._retrieveXMLRecords(response.data.result);
     }).catch((error) => {
         console.log(error)
     });
 }
 
-RetrieveFiles.retrieveXMLRecords = function(ids) {
+RetrieveFiles._retrieveXMLRecords = function(ids) {
     ids.forEach(id => {
         const url = `${instance}/api/352714/update_sets/get_xml`;
         axios.post(
@@ -47,23 +47,23 @@ RetrieveFiles.retrieveXMLRecords = function(ids) {
                 },
             }
         ).then(response => {
-            this.getXML(id, response.data);
+            this._getXML(id, response.data);
         }).catch(error => { 
             console.log(error);
         });
     });
 }
 
-RetrieveFiles.getXML = function(fileData, xmlData) {
+RetrieveFiles._getXML = function(fileData, xmlData) {
     parseString(xmlData, (err, result) => {
         xmlString = result.response.result[0].xmlDoc[0].documentElement[0];
         let encodedString = xmlString.replace('UTF-16', 'UTF-8');
         let fileName = fileData.name + '.xml';
-        this.writeFileToDir(fileName, fileData.sys_created_by, encodedString);
+        this._writeFileToDir(fileName, fileData.sys_created_by, encodedString);
     })
 }
 
-RetrieveFiles.writeFileToDir = function(fileName, userPath, xmlData) {
+RetrieveFiles._writeFileToDir = function(fileName, userPath, xmlData) {
     const dirPath = path.join(__dirname, '/update_set_exports');
     const usersPath = `${dirPath}/${userPath}`;
 
@@ -77,5 +77,50 @@ RetrieveFiles.writeFileToDir = function(fileName, userPath, xmlData) {
     });
 }
 
-RetrieveFiles.getFilesByUser('admin');
+RetrieveFiles._readFilesFromDirectory = function(user) {
+    const filesPath = path.join(__dirname, `/update_set_exports/${user}`);
+    return fs.readdirSync(filesPath);
+}
+
+// RetrieveFiles.getFilesByUser('admin');
+
+RetrieveFiles.sendToGitLab = function(user, files, projectId) {
+    const filesPath = path.join(__dirname, `/update_set_exports/${user}`);
+    const url = `https://gitlab.com/api/v4/projects/${projectId}/repository/commits`
+    var request = {
+        branch: 'dev',
+        commit_message: 'Committing SN Update Sets',
+        actions: this._buildJSON(filesPath, files)
+    };
+    axios.post(
+        url,
+        request,
+        {
+            headers: {
+                Accept: 'Application/json',
+                'Content-Type': 'Application/json',
+                PRIVATE_TOKEN: process.env.PRIVATE_TOKEN
+            },
+        }
+    ).then((response) => {
+        console.log(response);
+    }).catch(error => { 
+        console.log(error);
+    })
+}
+
+RetrieveFiles._buildJSON = function(path, files) {
+    let actions = [];
+    for(let i = 0; i < files.length; i++) {
+        var obj = {
+            action: 'create',
+            file_path: `${path}/${files[i]}`,
+            content: `${files[i]}`,
+        }
+        actions.push(obj);
+    }
+    return actions;
+}
+
+RetrieveFiles.sendToGitLab('admin', RetrieveFiles._readFilesFromDirectory('admin'), '31558220');
 
